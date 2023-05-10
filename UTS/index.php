cbe511797e469687bedfa4fd2c0eb2476975b7d1
dewiiratri21 <129348001+dewiiratri21@users.noreply.github.com>
2023-05-10 19:16:50 +0700

    Add files via upload
    UTS

---
+++ UTS/index.php
@@ -0,0 +1,438 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="assets/bootstrap-5.0.2-dist/css/bootstrap.min.css"
+    />
+    <title>Document</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            src="assets\img\logo restoran.png"
+            alt=""
+            width="30"
+            height="24"
+            class="d-inline-block align-text-top"
+          />
+          Restauranty
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Menu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Reservation</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">FAQS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Account</a>
+            </li>
+          </ul>
+          <button class="btn btn-dark" type="submit">Contact Now</button>
+        </div>
+      </div>
+    </nav>
+    <div class="container my-5">
+      <div class="p-5 text-center bg-body-tertiary rounded-3">
+        <svg
+          class="bi mt-4 mb-3"
+          style="color: var(--bs-indigo)"
+          width="100"
+          height="100"
+        >
+          <use xlink:href="#bootstrap" />
+        </svg>
+        <img src="assets/img/logo restoran.png" style="width:70%; height:70%;">
+        <h1 class="text-body-emphasis">Organic Foods Made Easy & Healty</h1>
+        <p class="col-lg-8 mx-auto fs-5 text-muted">
+          Organic Foods Made Easy & Healty
+        </p>
+        <form class="d-flex pb-5">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search Foods"
+            aria-label="Search"
+          />
+          <button class="btn btn-dark" type="submit">Search</button>
+        </form>
+        <div class="d-inline-flex gap-2 mb-5">
+          <p class="fw-bold">24/7 Fresh Food</p>
+          <p class="fw-bold">Nature Fresh</p>
+          <p class="fw-bold">Home Services</p>
+          <p class="fw-bold">Free Delivery</p>
+        </div>
+      </div>
+    </div>
+    <section>
+
+    <form class="d-flex pb-5" style="padding-left:3%">
+    <h5 style="margin-right:7%">Book a table</h5>      
+    <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search Foods"
+            aria-label="Search"
+            style="width:20%;
+          height:5%;"
+          />
+          <select
+            class="form-control me-2"
+            type="search"
+            placeholder="Search Foods"
+            aria-label="Search"
+            style="width:20%;
+          height:5%;"
+          >
+          <option></option>
+          <option></option>
+          <option></option>
+          </select>
+          <select
+            class="form-control me-2"
+            type="search"
+            placeholder="Search Foods"
+            aria-label="Search"
+            style="width:20%;
+          height:5%;"
+            >
+            <option></option>
+            <option></option>
+            <option></option>
+          </select>
+
+          <button class="btn btn-dark" type="submit">Contact Now</button>
+          </div>
+        </form>
+          </section>
+
+    <section>
+      <div class="p-5 text-center bg-body-tertiary rounded-3">
+        ;
+        <h1 class="text-body-emphasis">Choose & Enjoy</h1>
+        <p class="text-body-emphasis">
+          Inspire by Recipe and creations of worlds best chefs
+        </p>
+
+        <div class="row d-inline-flex gap-2 justify-content-md-center">
+          <div class="card" style="width: 18rem">
+            <img
+              src="assets/img/chicken biryani.jpeg"
+              class="card-img-top"
+              alt="..."
+              style="object-fit: cover; height: 200px"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Chicken Briyani</h5>
+              <p class="card-text">
+                
+              </p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <img
+              src="assets/img/mutton biryani.jpeg"
+              class="card-img-top"
+              alt="..."
+              style="object-fit: cover; height: 200px"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Mutton Briyani</h5>
+              <p class="card-text">
+                
+              </p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <img
+              src="assets/img/mutton karahi.jpeg"
+              class="card-img-top"
+              alt="..."
+              style="object-fit: cover; height: 200px"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Mutton Karahi</h5>
+              <p class="card-text">
+                
+              </p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <img
+              src="assets/img/chicken korma.jpeg"
+              class="card-img-top"
+              alt="..."
+              style="object-fit: cover; height: 200px"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Chicken Korma</h5>
+              <p class="card-text">
+                
+              </p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <img
+              src="assets/img/chicken Nirahi.jpeg"
+              class="card-img-top"
+              alt="..."
+              style="object-fit: cover; height: 200px"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Chicken Nihari</h5>
+              <p class="card-text">
+                
+              </p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <img
+              src="assets/img/arabic salad.jpeg"
+              class="card-img-top"
+              alt="..."
+              style="object-fit: cover; height: 200px"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Arabic Salad</h5>
+              <p class="card-text">
+                
+              </p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <img
+              src="assets/img/samosa.jpeg"
+              class="card-img-top"
+              alt="..."
+              style="object-fit: cover; height: 200px"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Samosa</h5>
+              <p class="card-text">
+                
+              </p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem">
+            <img
+              src="assets/img/rosmalai.jpeg"
+              class="card-img-top"
+              alt="..."
+              style="object-fit: cover; height: 200px"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Rosmalai</h5>
+              <p class="card-text">
+                
+              </p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+
+          <button class="btn btn-dark" type="submit">View All</button>
+        </div>
+      </div>
+    </section>
+    <section style="background-color: aliceblue;">
+      <div class="container">
+        <div class="row pt-5 pb-5">
+          <div class="col-md-6">
+            <h4>Special Offer</h4>
+            <h1>50% OFF</h1>
+            <p></p>
+            <h4>Use Cuopun : 50 OFF</h4>
+            <button class="btn btn-dark" type="submit">Order Now</button>
+          </div>
+          <div class="col-md-6">
+            <img src="assets/img/50 off.jpeg" style="width: 100%;" alt="" >
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container marketing">
+        <div class="p-5 text-center bg-body-tertiary rounded-3">
+        <h1 class="text-body-emphasis">Why Choose Us?</h1>
+        <div class="row">
+          <div class="col-lg-3">
+            <img class="" src="assets\img\koper.png" alt="" width="40" height="40">
+            <h2 style="font-size:medium">10+ Years of Experience</h2>
+            <p></p>
+            
+          </div><!-- /.col-lg-3 -->
+          <div class="col-lg-3">
+            <img class="" src="assets\img\person.png" alt="Generic placeholder image" width="40" height="40">
+            <h2 style="font-size:medium">200+ Professional Chefs</h2>
+            <p></p>
+            
+          </div><!-- /.col-lg-3 -->
+          <div class="col-lg-3">
+            <img class="" src="assets\img\person 2.png" alt="Generic placeholder image" width="40" height="40">
+            <h2 style="font-size:medium">5000+ Hppy Customers</h2>
+            <p></p>
+            
+          </div><!-- /.col-lg-3 -->
+          <div class="col-lg-3">
+            <img class="" src="assets\img\piala.png" alt="Generic placeholder image" width="40" height="40">
+            <h2 style="font-size:medium">20+ Awards Won</h2>
+            <p></p>
+        </div><!-- /.row -->
+      </div>
+    </section>
+
+    <section style="background-color:burlywood; padding-top: 20px; padding-bottom: 20px;">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">  
+    <div class="carousel-inner">
+    <div class="container">
+    
+        <div class="row">
+          <div class="col-md-4" >
+            <div class="card mb-4 box-shadow">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Card image cap" width="80" height="80">
+              <div class="card-body">
+                <p class="card-text fw-bold">John Doe</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  
+                  <small class="text-muted">I order food from there, they have very quick service, and taste is so good. I love it</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Card image cap" width="80" height="80">
+              <div class="card-body">
+                <p class="card-text fw-bold">John Smith</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  
+                  <small class="text-muted">I order food from there, they have very quick service, and taste is so good. I love it</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Card image cap" width="80" height="80">
+              <div class="card-body">
+                <p class="card-text fw-bold">Pamela John</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  
+                  <small class="text-muted">I order food from there, they have very quick service, and taste is so good. I love it</small>
+                </div>
+              </div>
+          </div>
+              <button class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev" >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+</button>
+        <button class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+</button>
+        </div>
+        </div>
+      </div>
+            </div>
+          </div>
+    </section>
+
+    <div class="container">
+      <footer class="row row-cols-5 py-5 my-5 border-top">
+    
+        <div class="col">
+          <h5>Popular Page</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About Us</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Our Chats</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Our Blog</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Contact Us</a></li>
+          </ul>
+        </div>
+    
+        <div class="col">
+          <h5>Popular Category</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Chicken</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Mushrom</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Rice</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Vegetarian</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Refreshment</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"></a></li>
+          </ul>
+        </div>
+    
+        <div class="col">
+          <h5>Reach us!</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          </ul>
+        </div>
+    
+        <div class="col text-center">
+          <h5>Connect with us</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><img class="" src="assets\img\fb.png" alt="" width="40" height="40"><a href="#" class="nav-link p-0 text-muted"></a></li>
+            <li class="nav-item mb-2"><img class="" src="assets\img\twitter.png" alt="" width="40" height="40"><a href="#" class="nav-link p-0 text-muted"></a></li>
+            <li class="nav-item mb-2"><img class="" src="assets\img\google+.png" alt="" width="40" height="40"><a href="#" class="nav-link p-0 text-muted"></a></li>
+            <li class="nav-item mb-2"><img class="" src="assets\img\ig.png" alt="" width="40" height="40"><a href="#" class="nav-link p-0 text-muted"></a></li>
+          </ul>
+        </div>
+      </footer>
+      <div class="container">
+        <p>&copy; 2019 Restauranty. All Rights Reserved.</p>
+        </div>
+    </div>
+  </body>
+</html>
